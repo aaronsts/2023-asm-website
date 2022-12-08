3bbcc4a5e4237051b8455c3117b6a5426c43cd47
Aaron Staes <aaronstaes@Aarons-MacBook-Air.local>
2022-12-08 08:54:27 +0100

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
 const Header = (): JSX.Element => {
-	return <nav className="container mx-auto">Test</nav>;
+	return (
+		<nav className="container mx-auto px-2 flex justify-between items-center h-16">
+			<div>Icon</div>
+			<ul className="flex gap-2">
+				<li>home</li>
+				<li>portfolio</li>
+				<li>about</li>
+				<li>contact</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Header;
