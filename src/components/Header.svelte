05ae1da6a9ed1f389c80316996136c9d3cd0a06c
Aaron Staes <70277183+aaronsts@users.noreply.github.com>
2023-02-06 17:09:31 +0100

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="fixed left-0 right-0 z-10 bg-slate-50/80 shadow backdrop-blur-sm ">
-	<div class="wrapper flex justify-between py-2">
+	<div class="wrapper flex justify-between py-4">
 		<Icon />
 		<ul class="flex items-center gap-2">
 			<li class="px-1 text-lg"><a href="/">home</a></li>
